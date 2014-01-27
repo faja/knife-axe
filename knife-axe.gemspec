41aea407f72b4a4837e444e7a9a9e76bc1732c0f
Sam Clements <sam.clements@datasift.com>
2014-01-27 10:19:01 +0000

    Packaged as a ruby gem
    Loads "chef/knife/core/object_loader" for each plugin, as this
    isn't loaded automatically for gem knife plugins

---
+++ knife-axe.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+	s.name = "knife-axe"
+	s.version = "0.0.0"
+	s.date = "2014-01-27"
+	s.summary = ""
+	s.description = ""
+	s.authors = ["Marcin Cabaj", "Sam Clements"]
+	s.email = ["marcin.cabaj@datasift.com", "sam.clements@datasift.com"]
+	s.homepage = "https://github.com/faja/knife-axe"
+	s.license = ""
+	s.files = Dir.glob("lib/**/*") + %w(README.md)
+	s.add_dependency "diffy", "~> 3.0.1"
+end
---
+++ lib/chef/knife/axe_env.rb
@@ -0,0 +1,54 @@
+class Chef
+	class Knife
+		class AxeEnv < Chef::Knife
+			deps do
+				require 'json'
+				require 'diffy'
+			end
+
+			banner 'knife axe env FILENAME'
+
+			def yesno
+				puts "Continue? [y/n]"
+				yn = STDIN.gets.chomp()
+				if yn == 'y' or yn == 'Y'
+					return 0
+				elsif yn == 'n' or yn == 'N'
+					puts "You chose 'n'. I'm done here."
+					exit 0
+				else
+					yesno
+				end
+			end
+
+			def run
+
+				if @name_args[0].nil?
+					show_usage
+					ui.fatal("You must specify a environment file")
+					exit 1
+				end
+
+				env_file = @name_args[0]
+				env_name = File.basename(env_file).split('.')[0]
+				loader = Chef::Knife::Core::ObjectLoader.new(Chef::Environment, ui)
+
+				env_ff = loader.load_from("environments", env_file)
+				pretty_json_env_ff = JSON.pretty_generate(env_ff.to_hash)
+
+				env_fc = Chef::Environment.load(env_ff.to_hash["name"])
+				pretty_json_env_fc = JSON.pretty_generate(env_fc.to_hash)
+
+				Diffy::Diff.default_format = :color
+				puts "You are going to update environment #{env_name}"
+				puts 'Here comes diff:'
+				puts Diffy::Diff.new(pretty_json_env_fc, pretty_json_env_ff, :context => 1)
+
+				puts
+				yesno
+				env_ff.save
+
+			end
+		end
+	end
+end
